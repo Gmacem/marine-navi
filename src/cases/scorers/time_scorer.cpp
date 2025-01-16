a5174643dd611c977cb4e336181005cda79133ac
Semyon Trifochkin <gmacem@inbox.ru>
2025-01-16 01:00:12 +0000

    fix

--- src/cases/scorers/time_scorer.cpp
+++ src/cases/scorers/time_scorer.cpp
@@ -1,9 +1,23 @@
 #include "time_scorer.h"
 
+#include "common/marine_math.h"
+
 namespace marine_navi::cases::scorers {
 
 namespace {
   constexpr double kMinRad = 0.1;
+
+std::tuple<common::Polygon, common::Polygon> MakeDepthCheckPolygon(const common::Segment& segment, double alpha) {
+  const auto direction = segment.End - segment.Start;
+  const auto poly1 = common::Polygon{
+    {segment.Start, segment.End, segment.Start + direction.Rotate(alpha)},
+  };
+  const auto poly2 = common::Polygon{
+    {segment.Start, segment.End, segment.Start + direction.Rotate(-alpha)}, 
+  };
+
+  return {poly1, poly2};
+}
 
 } // namespace
 
@@ -26,6 +40,23 @@ int64_t TimeScorer::GetScore(int start_id, int end_id, time_t depart_time) {
     wave_height = forecast->GetWaveHeight();
   }
   double speed = helpers::GetSpeed(ship_performance_info_, wave_height);
+  
+  {
+    // TODO: make function
+    std::vector<common::Polygon> polygons;
+    const auto alpha = common::CalculateSteeringAngle(speed);
+    const auto [poly1, poly2] = MakeDepthCheckPolygon({
+      .Start = start_point,
+      .End = end_point
+    }, alpha);
+
+    polygons.push_back(poly1);
+    polygons.push_back(poly2);
+    auto hazard_points = db_client_->SelectHazardDepthPoints(polygons, ship_performance_info_.DangerHeight.value());
+    if (hazard_points[0].size() > 0) {
+      return kMaxScore;
+    }
+  }
 
   return common::GetHaversineDistance(start_point, end_point) / speed;
 }
